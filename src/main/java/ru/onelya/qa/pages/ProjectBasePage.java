81744a06d17f45c09ec2aae64c3d253d0f402303
AlexeyMkr <alexeymkr@gmail.com>
2018-10-22 23:37:25 +0300

    - CreditCardDetailsPage update

--- src/main/java/ru/onelya/qa/pages/ProjectBasePage.java
+++ src/main/java/ru/onelya/qa/pages/ProjectBasePage.java
@@ -1,7 +1,12 @@
 package ru.onelya.qa.pages;
 
 import com.wiley.page.BasePage;
+import org.openqa.selenium.By;
+import ru.onelya.qa.elements.TextField;
 
 public class ProjectBasePage extends BasePage {
 
+    protected TextField textField(By by) {
+        return new TextField(by);
+    }
 }
--- src/main/java/ru/onelya/qa/pages/credit_card_validation/CreditCardDetailsPage.java
+++ src/main/java/ru/onelya/qa/pages/credit_card_validation/CreditCardDetailsPage.java
@@ -2,6 +2,7 @@ package ru.onelya.qa.pages.credit_card_validation;
 
 import com.wiley.elements.TeasyElement;
 import org.openqa.selenium.By;
+import ru.onelya.qa.elements.TextField;
 import ru.onelya.qa.pages.ProjectBasePage;
 
 public class CreditCardDetailsPage extends ProjectBasePage {
@@ -12,6 +13,7 @@ public class CreditCardDetailsPage extends ProjectBasePage {
     private static final By CREDIT_CARD_EXPIRATION_MONTH_LOCATOR = By.cssSelector("#expiration_month");
     private static final By CREDIT_CARD_EXPIRATION_YEAR_LOCATOR = By.cssSelector("#expiration_year");
     private static final By CREDIT_CARD_PAYMENT_AMOUNT_LOCATOR = By.cssSelector("#amount");
+    private static final By CREDIT_CARD_SUBMIT_BUTTON_LOCATOR = By.cssSelector("#submit");
     private static final By CREDIT_CARD_ERROR_LOCATOR = By.cssSelector(".error");
 
     public CreditCardDetailsPage checkCreditCardFormElementIsDisplayed() {
@@ -44,8 +46,23 @@ public class CreditCardDetailsPage extends ProjectBasePage {
         return this;
     }
 
+    public CreditCardDetailsPage checkCreditCardSubmitButtonIsDisplayed() {
+        getCreditCardSubmitButtonElement().should().beDisplayed();
+        return this;
+    }
+
+    public CreditCardDetailsPage clickCreditCardSubmitButton() {
+        getCreditCardSubmitButtonElement().click();
+        return this;
+    }
+
     public CreditCardDetailsPage checkCreditCardErrorElementIsAbsent() {
         getCreditCardErrorElement().should().beAbsent();
+        return this;
+    }
+
+    public CreditCardDetailsPage checkCreditCardErrorElementIsDisplayed() {
+        getCreditCardErrorElement().should().beDisplayed();
         return this;
     }
 
@@ -53,24 +70,28 @@ public class CreditCardDetailsPage extends ProjectBasePage {
         return element(CREDIT_CARD_FORM_LOCATOR);
     }
 
-    private TeasyElement getCreditCardNumberElement() {
-        return element(CREDIT_CARD_NUMBER_LOCATOR);
+    private TextField getCreditCardNumberElement() {
+        return textField(CREDIT_CARD_NUMBER_LOCATOR);
     }
 
-    private TeasyElement getCreditCardCvvElement() {
-        return element(CREDIT_CARD_CVV_LOCATOR);
+    private TextField getCreditCardCvvElement() {
+        return textField(CREDIT_CARD_CVV_LOCATOR);
     }
 
-    private TeasyElement getCreditCardExpirationMonthElement() {
-        return element(CREDIT_CARD_EXPIRATION_MONTH_LOCATOR);
+    private TextField getCreditCardExpirationMonthElement() {
+        return textField(CREDIT_CARD_EXPIRATION_MONTH_LOCATOR);
     }
 
-    private TeasyElement getCreditCardExpirationYearElement() {
-        return element(CREDIT_CARD_EXPIRATION_YEAR_LOCATOR);
+    private TextField getCreditCardExpirationYearElement() {
+        return textField(CREDIT_CARD_EXPIRATION_YEAR_LOCATOR);
     }
 
-    private TeasyElement getCreditCardAmountElement() {
-        return element(CREDIT_CARD_PAYMENT_AMOUNT_LOCATOR);
+    private TextField getCreditCardAmountElement() {
+        return textField(CREDIT_CARD_PAYMENT_AMOUNT_LOCATOR);
+    }
+
+    private TeasyElement getCreditCardSubmitButtonElement() {
+        return element(CREDIT_CARD_SUBMIT_BUTTON_LOCATOR);
     }
 
     private TeasyElement getCreditCardErrorElement() {
