a5ceba4dbcb5d4b21cd176d716dff7e305612b8e
Alexey Makarenko <amakarenko@wiley.com>
2018-10-22 11:43:25 +0300

    Core functionality development - multi-environment support added

---
+++ src/main/java/ru/onelya/qa/base/Environment.java
@@ -0,0 +1,20 @@
+package ru.onelya.qa.base;
+
+public enum Environment {
+
+    DEFAULT {
+        @Override
+        public EnvironmentProperties getProperties() {
+            return EnvironmentProperties.get("/default.properties");
+        }
+    };
+
+    public static Environment get() {
+        switch (System.getProperty("run.environment")) {
+            default:
+                return DEFAULT;
+        }
+    }
+
+    public abstract EnvironmentProperties getProperties();
+}
---
+++ src/main/java/ru/onelya/qa/base/EnvironmentProperties.java
@@ -0,0 +1,25 @@
+package ru.onelya.qa.base;
+
+import java.io.IOException;
+import java.util.Properties;
+
+public class EnvironmentProperties extends Properties {
+
+    private static EnvironmentProperties environmentProperties;
+
+    static EnvironmentProperties get(String filePath) {
+        if (environmentProperties == null) {
+            try {
+                environmentProperties = new EnvironmentProperties();
+                environmentProperties.load(EnvironmentProperties.class.getResourceAsStream("/properties/environment" + filePath));
+            } catch (IOException e) {
+                throw new RuntimeException("Can't load properties file by path [/properties/environment" + filePath + "]");
+            }
+        }
+        return environmentProperties;
+    }
+
+    public String getCreditCardFormUrl() {
+        return getProperty("CREDIT_CARD_FORM_URL");
+    }
+}
